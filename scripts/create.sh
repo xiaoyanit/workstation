41c5a67fa84c2f7375934181d097a77b35984fd8
Kamil Olesiejuk <kamil@novoda.com>
2010-08-17 14:06:45 +0100

    Added scripts directory and an interactive script that creates an Android project and a instrunemtation for it.

---
+++ scripts/create.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+echo "Create an Android Project."
+read -p "Enter project name: " name
+echo "Enter project package:"
+read -p "    novoda." pkg
+echo "Enter target id."
+android list targets | grep 'id:'
+read -p "Chosen id (default: 10):" target
+read -p "Enter default Activity name (Landing):" activity
+
+<<TEST1
+if [ -n "$target" ]
+	then 
+	target=10
+fi
+
+if [ -n "$activity" ]
+	then 
+	activity=Landinng	
+fi
+TEST1
+
+package=novoda.$pkg
+testname=$name\Test
+dir=`pwd`
+lowname=`echo "TasteLondon" | tr "[A-Z]" "[a-z]"`
+main=$dir/$lowname/$name
+
+echo "Create test project with details"
+echo "Name: $name"
+echo "package: $package"
+echo "Test name: $testname"
+echo "target: $target"
+echo "Activity: $activity"
+
+mkdir $lowname
+cd $lowname
+
+echo "Creating Android project"
+android -v create project -t $target -n $name -p ./$name -a $activity -k $package
+
+echo "Creating test project"
+android -v create test-project -n $testname  -p ./$testname -m $main
+
+exit $?
